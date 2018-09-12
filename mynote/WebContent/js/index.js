da5a7466f8fb397f4430bbebf08efba798d64cf9
juanjorgegarcia <juan.j.garcia007@gmail.com>
2018-09-12 08:37:12 -0300

    edit note feito com fetch

--- mynote/WebContent/js/index.js
+++ mynote/WebContent/js/index.js
@@ -19,32 +19,35 @@ function alou(idNote){
 	let note = document.getElementById("editor"+idNote)
 	note.addEventListener("input", debounce(()=>{
 		console.log('atualizando o bd')
-		let xhr = new XMLHttpRequest();
+// let xhr = new XMLHttpRequest();
 		const url = "/mynote/EditNoteText"
 		let noteText = note.innerText
-//		let params = "noteText=" + encodeURIComponent(noteText)+"idNote=" + encodeURIComponent(idNote)
+// let params = "noteText=" + encodeURIComponent(noteText)+"idNote=" +
+// encodeURIComponent(idNote)
 		let params = {
 				"noteText":encodeURIComponent(noteText),
 				"idNote":encodeURIComponent(idNote)
 		}
 		console.log(idNote)
-//		fetch(url, {
-//		    method : "POST",
-//		    body: params,
-//
-//		}).then(
-//			    response => response.text() // .json(), etc.
-//			    // same as function(response) {return response.text();}
-//			).then(
-//			    html => console.log(html)
-//			);
+		console.log('request enviado')
+		fetch(url, {
+		    method : "POST",
+		    body: 'note='+JSON.stringify(params),
+		    headers:    {
+		        "Content-Type": "application/x-www-form-urlencoded"
+		    }
 
-		xhr.open("POST", url);
-		xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-		xhr.send('note='+(JSON.stringify(params)));
+
+		}).then(
+			    console.log('request enviado') // .json(), etc.
+			    // same as function(response) {return response.text();}
+			)
+// xhr.open("POST", url);
+// xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+// xhr.send('note='+(JSON.stringify(params)));
 		
 
-//		console.log(document.getElementByClassName('noteText')[0].innerText)
+// console.log(document.getElementByClassName('noteText')[0].innerText)
 	},1000*2), false);
 }
 document.addEventListener('DOMContentLoaded', function() {
